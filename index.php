e1c7bcc183e84bcd6bc6fcbca752dd218a728d42
skidviciousrsb <60114701+skidviciousrsb@users.noreply.github.com>
2023-02-13 11:47:56 -0300

    Add files via upload
    Adicionando arquivos iniciais

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Plataforma EAD</title>
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+  </head>
+  <body>
+    <header class="cabecalho">
+      <nav class="menu">
+        <h1 class="logo">Plataforma EAD</h1>
+        <div class="botoes">
+          <a href="#" class="botao">Cadastre-se</a>
+          <a href="#" class="botao">Login</a>
+        </div>
+      </nav>
+    </header>
+    <main class="principal">
+      <h2>Bem-vindo à plataforma EAD</h2>
+      <p>Acesse nossa ampla biblioteca de cursos online e aproveite nossas aulas em video.</p>
+      <a href="#" class="botao">Ver cursos disponíveis</a>
+    </main>
+    <footer class="rodape">
+      <p>Plataforma EAD - Todos os direitos reservados</p>
+    </footer>
+  </body>
+</html>
